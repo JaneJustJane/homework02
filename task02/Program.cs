8aa7b799093f7d9411d7f587974ca7c7b3567997
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-19 12:38:56 +0300

    sum from 1 to A

---
+++ task02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("введите число a");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+int sum=0;
+while (index <= a) 
+{
+    sum = sum + index;
+    index++;
+}
+Console.WriteLine($"Сумма чисел от 1 до {a} равна {sum}");
