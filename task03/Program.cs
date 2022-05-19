1c74e9143a14f4afa181f3f4f0b18b08a7fe2b1c
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-19 13:00:42 +0300

    a^b

---
+++ task03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine ("введите число А");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine ("введите степень В, в которую Вы хотите  возвести число");
+int b = int.Parse(Console.ReadLine() ?? "0");
+int result = 1;
+for (int i=1; i<=b; i++)
+{
+    result = result*a;
+}
+Console.Write ($"число {a} в степени {b} равно {result}");
