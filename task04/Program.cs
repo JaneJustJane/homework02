1fc4aa20802a38e9d90ea20e62fc0cf32cb1dd4a
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-19 13:17:26 +0300

    sum of digits

---
+++ task04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("введите число a");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int count = 0;
+Console.WriteLine ($"сумма цифр в числе {a} =");
+while (a>0)
+{
+    count = count + 1;
+    a=a/10;
+}
+
+Console.Write($" = {count}");
