eafa25310929f9e797b45009c2b428a1c3f2b1dc
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-19 12:20:19 +0300

    sq of the num from 1 to N

---
+++ task00/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("введите число n");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+
+while (index <= n) 
+{
+    Console.Write (index*index + " ");
+    index++;
+}
