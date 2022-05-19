4b14e6cd0888882873bbb8a417c8bd2e57c7f211
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-19 13:55:03 +0300

    cubics of even nmbrs

---
+++ task05/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("введите число n");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int index = 2;
+Console.WriteLine ($"Кубы чисел от 1 до {n}, заканчивающихся на четную цифру равны");
+while (index <= n) 
+{
+    Console.Write (index*index*index + " ");
+    index=index+2;
+}
+
+//int result = 1;
+//if (n%2==0)
+//{
+   // result = n*n*n;
+   // Console.WriteLine ($"Куб числа {n} = {result}");
+//}
+//else
+//{
+   // Console.WriteLine($"Число {n} нечетное");
+//}
