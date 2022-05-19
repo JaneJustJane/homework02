177dc37a2fd2d7f75cf508525375443e0c9e6c88
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-19 12:30:55 +0300

    cubic of the num from 1 to N

---
+++ task01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("введите число n");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+Console.WriteLine ($"Кубы чисел от 1 до {n} равны");
+while (index <= n) 
+{
+    Console.Write (index*index*index + " ");
+    index++;
+}
